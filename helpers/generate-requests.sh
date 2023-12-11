42c6e76b53872fe60223de51304c7d0abc410c2f
Alex Runyan <alex@runyan.co>
2023-12-11 13:10:54 -0500

    Updates

--- helpers/generate-requests.sh
+++ helpers/generate-requests.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 
 {
-  set -e
   source ./.env
 
+  export URL=""
+  export COOKIE=""
+
+  randomString() {
+    php ./random-string.php;
+  }
+
   sendHttpRequest() {
-    true
+    timeout 5 curl "$URL" \
+      -H 'Accept: application/json, text/plain, */*' \
+      -H 'Accept-Language: en' \
+      -H 'Connection: keep-alive' \
+      -H 'Content-Type: application/json' \
+      -H 'Cookie: ' \
+      -H 'DNT: 1' \
+      -H 'Origin: http://processmaker.test' \
+      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36' \
+      -H 'X-Requested-With: XMLHttpRequest' \
+      --data-raw '{}' \
+      --compressed \
+      --insecure \
+      --silent >/dev/null && echo "Done: $2" && exit 0
   }
 
   while true; do
-    for i in {0..32}; do
-      { echo "$i" && sendHttpRequest >/dev/null; } &
+    for i in {0..16}; do
+      { echo "Starting $i" && sendHttpRequest "$(randomString)" "$i"; } &
     done
     wait
+    sleep 1
   done
 }
